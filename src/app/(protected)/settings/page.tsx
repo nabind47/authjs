bd429574334a3112c0ae4b29e19f9e71e255a338
nabind47 <nabindhami619@gmail.com>
2024-01-27 09:41:28 +0545

    feat: protected layout

--- src/app/(protected)/settings/page.tsx
+++ src/app/(protected)/settings/page.tsx
@@ -1,21 +1,5 @@
-import { auth, signOut } from "@/auth";
-
-const Settings = async () => {
-  const session = await auth();
-
-  return (
-    <div>
-      {session && JSON.stringify(session)}
-      <form
-        action={async () => {
-          "use server";
-          await signOut();
-        }}
-      >
-        <button type="submit">Logout</button>
-      </form>
-    </div>
-  );
+const Settings = () => {
+  return <div></div>;
 };
 
 export default Settings;
---
+++ src/components/auth/user-button.tsx
@@ -0,0 +1,39 @@
+"use client";
+
+import { ExitIcon } from "@radix-ui/react-icons";
+import { FaUser } from "react-icons/fa";
+
+import { LogoutButton } from "@/components/auth/logout-button";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { useCurrentUser } from "@/hooks/use-current-user";
+
+export const UserButton = () => {
+  const user = useCurrentUser();
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger>
+        <Avatar>
+          <AvatarImage src={user?.image || ""} />
+          <AvatarFallback className="bg-sky-500">
+            <FaUser className="text-white" />
+          </AvatarFallback>
+        </Avatar>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="w-40" align="end">
+        <LogoutButton>
+          <DropdownMenuItem>
+            <ExitIcon className="h-4 w-4 mr-2" />
+            Logout
+          </DropdownMenuItem>
+        </LogoutButton>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
